fe6245abb5a73c9f8338488905feeaa2cb4d3394
sasham43 <sashasemail@gmail.com>
2016-04-21 14:32:52 -0500

    GIF is all working

--- client/client.js
+++ client/client.js
@@ -1,7 +1,7 @@
 var app = angular.module('patronusApp', []);
 
-app.controller('PatronusController', ['$http', function($http){
-  console.log('angular set up');
+app.controller('PatronusController', ['$timeout', '$http', function($timeout, $http ){
+  // console.log('angular set up');
   var controller = this;
   controller.person = {};
   controller.patronus = {};
@@ -13,7 +13,7 @@ app.controller('PatronusController', ['$http', function($http){
   controller.patronusesID = 0;
 
   controller.savePerson = function(){
-    console.log(controller.person);
+    // console.log(controller.person);
     $http.post('/people', controller.person).then(function(serverResponse){
       console.log(serverResponse);
       controller.getPeople();
@@ -30,11 +30,11 @@ app.controller('PatronusController', ['$http', function($http){
   controller.getPeople = function(){
     $http.get('/people').then(function(response){
       controller.peopleList = response.data;
-      console.log('peopleList', controller.peopleList);
+      // console.log('peopleList', controller.peopleList);
       controller.unassignedPeopleList = controller.peopleList.filter(function(person){
         return person.patronus_id == null;
       });
-      console.log('unassignedPeopleList', controller.unassignedPeopleList);
+      // console.log('unassignedPeopleList', controller.unassignedPeopleList);
 
       controller.assignedPeopleList = controller.peopleList.filter(function(person){
         return person.patronus_id !== null;
@@ -49,12 +49,19 @@ app.controller('PatronusController', ['$http', function($http){
   };
 
   controller.assignPatronus = function() {
+    controller.showGIF = true;
+
     $http.put("/people/" + controller.peopleID, {patronus_id : controller.patronusesID}).then(function(response) {
       controller.getPatronuses();
       controller.getPeople();
+
+      // controller.showGIF = true;
+      $timeout(function(){
+        controller.showGIF = false;
+        // console.log("test", controller.showGIF);
+      }, 3000);
     });
   };
-
 
   controller.getPeople();
   controller.getPatronuses();
